057accdd027c908840bc2b82261f0eea35d53ca4
MbusyaNgesa <theengesa@gmail.com>
2024-03-05 22:42:18 +0300

    API for fetching all products

--- index.js
+++ index.js
@@ -79,6 +79,8 @@ const Product = mongoose.model("Product",{
     },
 })
 
+//API for adding products
+
 app.post('/addproduct',async (req,res)=>{
     let products = await Product.find({});
     let id;
@@ -119,6 +121,14 @@ app.post('/removeproduct', async(req,res)=>{
     })
     
 })
+
+// Creating API for getting all products
+app.get('/allproducts',async(req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
+
 app.listen(port, (error)=>{
     if (!error){
         console.log("Server Running on Port" +port)
